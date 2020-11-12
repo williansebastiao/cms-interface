80dfb88b27b2e63d97e31424837d191bda093975
Willians Pereira <willians@4vconnect.com>
2020-11-12 00:54:14 -0300

    feature: change route

--- src/router/user.js
+++ src/router/user.js
@@ -3,7 +3,7 @@ export default [
 		path: '/profile',
 		name: 'Profile',
 		icon: 'profile',
-		component: () => import('@/pages/profile/Personal'),
+		component: () => import('@/pages/profile/Password'),
 		children: [
 			{
 				path: '/profile/personal-information',

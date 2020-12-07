f9101d4b63fc2c452c2c171aa767fb9f62ab33e5
Willians Pereira <willians@4vconnect.com>
2020-12-07 16:12:53 -0300

    fix: remove route notifications from profile

--- src/router/user.js
+++ src/router/user.js
@@ -6,23 +6,17 @@ export default [
 		component: () => import('@/pages/profile/Personal')
 	},
 	{
-		path: '/profile/personal',
-		name: 'Personal Information',
-		icon: 'profile',
-		component: () => import('@/pages/profile/Personal')
-	},
-	{
 		path: '/profile/address',
 		name: 'Address Information',
 		icon: 'profile',
 		component: () => import('@/pages/profile/Address')
 	},
-	{
+	/*{
 		path: '/profile/notifications',
 		name: 'Notification Settings',
 		icon: 'bell',
 		component: () => import('@/pages/profile/Notifications')
-	},
+	},*/
 	{
 		path: '/profile/password',
 		name: 'Password Security',

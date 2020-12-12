26d7f27f41a1476551a65a55dc5ea70b2e1f4db0
Willians Pereira <willians@4vconnect.com>
2020-12-11 21:57:48 -0300

    fix: active menu on profile

--- src/router/index.js
+++ src/router/index.js
@@ -11,8 +11,8 @@ Vue.use(VueRouter)
 const router = new VueRouter({
 	routes: [...Auth, ...Menu, ...Static, ...Errors, ...User],
 	mode: 'history',
-	// linkExactActiveClass: 'is-active',
-	linkActiveClass: 'is-active'
+	linkExactActiveClass: 'is-active'
+	//linkActiveClass: 'is-active'
 })
 
 export default router
